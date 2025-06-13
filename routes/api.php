50392278af2e27d01cb1ce424dff2a2f95d7ebcf
Hisham Muteb <muteb.643@gmail.com>
2025-06-13 18:17:35 +0300

    create migration, contoller, model promocode

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PromoCodeController;
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/test', function () {
+    return response()->json(['message' => 'Hello, API']);
+});
+
+Route::post("/promo-codes",[PromoCodeController::class, 'store']);
