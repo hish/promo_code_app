57aa5ff6467f39049f2d573eda4cc8e2c21f509d
Hisham Muteb <muteb.643@gmail.com>
2025-06-14 19:14:23 +0300

    user authountcation

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string",
+            "email" => "required|string|email|unique:users",
+            "password" => "required|confirmed" // password_confirmation
+        ]);
+ 
+        if ($validator->fails()) {
+            $errorMessage = $validator->errors()->first();
+            $response = [
+                'status'  => false,
+                'message' => $errorMessage,
+            ];
+            return response()->json($response, 401);
+        }
+ 
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password)
+        ]);
+ 
+        // Response
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "email" => "required",
+            "password" => "required"
+        ]);
+ 
+        if ($validator->fails()) {
+            $errorMessage = $validator->errors()->first();
+            $response = [
+                'status'  => false,
+                'message' => $errorMessage,
+            ];
+            return response()->json($response, 401);
+        }
+ 
+        // Check user by email
+        $user = User::where("email", $request->email)->first();
+ 
+        // Check user by password
+        if (!empty($user)) {
+ 
+            if (Hash::check($request->password, $user->password)) {
+ 
+                // Login is ok
+                $tokenInfo = $user->createToken("promo_code_app");
+ 
+                $token = $tokenInfo->plainTextToken; // Token value
+ 
+                return response()->json([
+                    "status" => true,
+                    "message" => "Login successful",
+                    "token" => $token
+                ]);
+            } else {
+ 
+                return response()->json([
+                    "status" => false,
+                    "message" => "Password didn't match."
+                ]);
+            }
+        } else {
+ 
+            return response()->json([
+                "status" => false,
+                "message" => "Invalid credentials"
+            ]);
+        }
+    }
+
+    // Profile (GET, Auth Token)
+    public function profile()
+    {
+        $userData = auth()->user();
+ 
+        return response()->json([
+            "status" => true,
+            "message" => "Profile information",
+            "data" => $userData
+        ]);
+    }
+ 
+    // Logout (GET, Auth Token)
+    public function logout()
+    {
+        // To get all tokens of logged in user and delete that
+        request()->user()->tokens()->delete();
+ 
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PromoCodeController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -12,5 +13,16 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::post('/test', function () {
     return response()->json(['message' => 'Hello, API']);
 });
+
+Route::post("/register", [UsersController::class, 'register']);
+Route::post("/login", [UsersController::class, 'login']);
+
+Route::group(["middleware" => ["auth:sanctum"]],
+    function(){
+        Route::get('/profile', [UsersController::class, 'profile']);
+        Route::get('/logout', [UsersController::class, 'logout']);
+    }
+);
+
 
 Route::post("/promo-codes",[PromoCodeController::class, 'store']);
